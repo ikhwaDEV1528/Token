fd4607eccb0e9e9f1a8ad42a68dcc4f15e511a7e
ikhwaDEV1528 <ikhwanmarditya123@student.esaunggul.ac.id>
2026-07-23 20:54:24 +0700

    Add export default app for Vercel

--- main_server.js
+++ main_server.js
@@ -1,22 +1,23 @@
 import express from 'express';
-import login from './endpoint/auth/login.js'
+import login from './endpoint/auth/login.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-
 const app = express();
+
 app.use(cors({
     origin: process.env.CLIENT_URL || 'http://localhost:3000',
-    credentials:true
+    credentials: true
 })); 
 
-
 app.use(express.json()); // Wajib ada supaya req.body tidak kosong!
-app.use(cookieParser())
+app.use(cookieParser());
 
+app.use('/server_login', login);
 
+// Untuk testing lokal di laptop kamu
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log('BE JALAN DI PORT ' + port));
 
-app.use('/server_login' , login)
-
-const port = process.env.PORT ||  4000 
-app.listen(port , ()=> console.log('BE JALAN DI PORT' + port))
+// ⚠️ WAJIB DI-EXPORT SUPAYA VERCEL BISA BACA APP EXPRESS KAMU
+export default app;
