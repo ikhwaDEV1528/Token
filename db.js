7735be9dc3438cf6cf78c1f9c2f0f94a44ff8e6b
ikhwaDEV1528 <ikhwanmarditya123@student.esaunggul.ac.id>
2026-09-15 23:06:42 +0700

    fix: replace manual cors middleware with official package

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: 'erlgq-9-154-220-124.run.pinggy-free.link',
+  host: 'whcmf-9-154-220-124.run.pinggy-free.link',
   user: 'root', 
-  port:39711,     // Username MySQL (XAMPP/Laragon default: root)
+  port:42265,     // Username MySQL (XAMPP/Laragon default: root)
   password: '',      // Password MySQL (default: kosong)
   database: 'cv_bakhti_putra_pratama', // Sesuaikan nama database kamu
   waitForConnections: true,
