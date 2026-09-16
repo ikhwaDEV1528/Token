8d2846d7e733fa122dffc6d566fad3f47ae6efce
ikhwaDEV1528 <ikhwanmarditya123@student.esaunggul.ac.id>
2026-09-16 20:59:58 +0700

    Fix Netlify deployment

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: 'eglet-9-154-220-124.run.pinggy-free.link',
+  host: 'igqrd-9-154-220-124.run.pinggy-free.link',
   user: 'root', 
-  port:33523,     // Username MySQL (XAMPP/Laragon default: root)
+  port:43165,     // Username MySQL (XAMPP/Laragon default: root)
   password: '',      // Password MySQL (default: kosong)
   database: 'cv_bakhti_putra_pratama', // Sesuaikan nama database kamu
   waitForConnections: true,
